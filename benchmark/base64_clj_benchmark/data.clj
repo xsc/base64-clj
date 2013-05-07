2ec5f86ad7effee18662821eaeeacda86c01b3ad
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-07 11:02:45 +0200

    Using Random Data for Benchmark.

--- benchmark/base64_clj_benchmark/data.clj
+++ benchmark/base64_clj_benchmark/data.clj
@@ -1,17 +1,11 @@
 (ns base64-clj-benchmark.data
   (:use criterium.core))
 
-(def s0
-  (str "Man is distinguished, not only by his reason, but by this singular passion from other "
-       "animals, which is a lust of the mind, that by a perseverance of delight in the continued "
-       "and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure."))
+;; ## Data
 
-(def s1  
-  (str "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz"
-       "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg"
-       "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu"
-       "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo"
-       "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4="))
+(defn random-data
+  [n]
+  (apply str (take n (map char (repeatedly #(rand-int 256))))))
 
 ;; ## Macro for Main Function
 
@@ -19,16 +13,18 @@
   [id encode-fn decode-fn]
   `(defn ~id
      [& args#]
-     (if (= (first args#) "--quick")
-       (do
-         (println ~(str "Quick Benchmarking `" encode-fn "` ..."))
-         (quick-bench (~encode-fn s0) :verbose)
-         (println)
-         (println ~(str "Quick Benchmarking `" decode-fn "` ..."))
-         (quick-bench (~decode-fn s1) :verbose))
-       (do
-         (println ~(str "Benchmarking `" encode-fn "` ..."))
-         (bench (~encode-fn s0) :verbose)
-         (println)
-         (println ~(str "Benchmarking `" decode-fn "` ..."))
-         (bench (~decode-fn s1) :verbose)))))
+     (let [d# (random-data 128)
+           e# (~encode-fn d#)]
+       (if (= (first args#) "--quick")
+         (do
+           (println ~(str "Quick Benchmarking `" encode-fn "` ..."))
+           (quick-bench (~encode-fn d#) :verbose)
+           (println)
+           (println ~(str "Quick Benchmarking `" decode-fn "` ..."))
+           (quick-bench (~decode-fn e#) :verbose))
+         (do
+           (println ~(str "Benchmarking `" encode-fn "` ..."))
+           (bench (~encode-fn d#) :verbose)
+           (println)
+           (println ~(str "Benchmarking `" decode-fn "` ..."))
+           (bench (~decode-fn e#) :verbose))))))
